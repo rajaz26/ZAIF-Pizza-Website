05821f3feb44132f72949b1c5901cb067bedefb0
rajaz26 <rajazain360.rz@gmail.com>
2023-08-16 06:41:07 +0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["res.ry.com"],
+//   },
+// };
+
+// module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.ry.com", "res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
